23e52bf2322806a69dc9aedb2af222c626cbfc86
狩俣佑太 <ignorantbeginner@gmail.com>
2017-09-26 16:57:58 +0900

    引数、戻り値３

---
+++ CampChallenge_java/011.メソッド/method09.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package method;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author karimatayuuta2
+ */
+public class method09 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    String[][]myprofile(){
+        //三人分のプロフィールを二次元配列でまとめる。
+        String[][]data=
+        {
+           {"1", "野比のび太", "8月7日", "東京"}, 
+            {"2", "骨川スネ夫", "3月28日", null},
+            {"3", "剛田武", "6月15日", "東京"},
+        };
+        return data;
+    }
+    //ここまでは、引数と戻り値２の値と同じ。
+    
+    
+    
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet method09</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet method09 at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+            
+            Integer limit = 2;
+            for(int i=0;i<limit;i++){    //配列を出す。
+            String[][] al= myprofile();//帰ってきたデータを入れる箱を作る。//配列を出す。
+            for(int j=1;j<4;j++){
+                if(al[i][j]==null){
+                    continue;
+                }
+                out.println(al[i][j]+"<br>");
+            }
+            }    
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
