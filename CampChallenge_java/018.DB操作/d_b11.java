853e9740bd1c5bd485227c8c988737b88995de4a
狩俣佑太 <ignorantbeginner@gmail.com>
2017-09-26 21:05:56 +0900

    DB操作（特定レコードの更新２）

---
+++ CampChallenge_java/018.DB操作/d_b11.java
@@ -0,0 +1,126 @@
+package DataOperation;
+
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class d_b11 extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            request.setCharacterEncoding("UTF-8");
+            
+            
+            Connection db_con = null;//DB接続するためにConnectionが必要
+            PreparedStatement db_st = null;//SQL文を表すためPreparedStatementが必要
+            ResultSet db_data = null;
+            
+            String user = "KARIMATA";
+            String password = "KARI";
+            String url = "jdbc:mysql://localhost:8889/geekjob_db";
+            
+            
+            int id1 = Integer.parseInt(request.getParameter("id"));
+
+            
+
+            
+
+            try {
+                Class.forName("com.mysql.jdbc.Driver").newInstance();
+                
+                db_con = DriverManager.getConnection(url, user, password);
+                out.println("DB接続完了" + "<br>");
+
+                db_st = db_con.prepareStatement("Update tab set name = ?,tel = ?,age = ?,birthday = ? where profilesID = ?");//条件によるレコードの削除
+                db_st.setString(1, "サザエさん");
+                db_st.setString(2, "090-1234-1234");
+                db_st.setInt(3,30);
+                db_st.setString(4,"1999-12-31");
+                db_st.setInt(5, id1);
+                
+                db_st.executeUpdate();
+                
+                db_st = db_con.prepareStatement("select * from tab");
+                db_data = db_st.executeQuery();//executeUpdateはSQL文を実行し、作成された結果を返すために必要
+                
+
+                while (db_data.next()) {
+                    out.println("id:" + db_data.getInt("profilesID"));
+                    out.println("name:" + db_data.getString("name"));
+                    out.println("tel:" + db_data.getString("tel"));
+                    out.println("age:" + db_data.getInt("age"));
+                    out.println("birthday:" + db_data.getString("birthday") + "<br>");
+
+                }
+                db_data.close();
+                db_st.close();
+                db_con.close();
+            } catch (SQLException e) {
+                out.println("SQLException:" + e.getMessage());
+            } catch (ClassNotFoundException e) {
+                out.println("ClassNotFoundException:" + e.getMessage() + "<br>");
+            } catch (Exception e) {
+                out.println("Exception:" + e.getMessage() + "<br>");
+            } finally {
+                if (db_con != null) {
+                    try {
+                        db_con.close();
+
+                        out.println("DBとの切断に成功");
+                    } catch (Exception e) {
+
+                        out.println(e.getMessage());
+                    }
+                }
+
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ CampChallenge_java/018.DB操作/d_b11html.jsp
@@ -0,0 +1,23 @@
+
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>d_b11送信用</title>
+    </head>
+    <body>
+        <form action ="./d_b11" method ="post">
+
+            指定するprofilesIDのレコードを入力してください<br>
+            profilesID<br>
+            <input type = "text" name = "id"><br>
+
+
+
+            <input type = "submit" name = "btnSubmit" value = "送信">
+
+      　</form>
+    </body>
+</html>
