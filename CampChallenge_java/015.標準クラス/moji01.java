90479196c7eea0033b398af18f6c751a45bac9f5
狩俣佑太 <ignorantbeginner@gmail.com>
2017-09-26 17:40:13 +0900

    文字列編（バイト数と文字数の取得）

---
+++ CampChallenge_java/015.標準クラス/moji01.java
@@ -0,0 +1,18 @@
+
+package moji;
+
+
+public class moji01 {
+    
+   public static void main(String[] args){
+       String simei = "狩俣佑太";
+       System.out.println(simei.length());
+       
+       int byteSize = simei.getBytes().length;
+       System.out.println(byteSize);
+       
+   }
+    
+    
+    
+}
