728d091b53d144a450d9a1c848d6c5771f256c2b
狩俣佑太 <ignorantbeginner@gmail.com>
2017-09-26 16:55:11 +0900

    引数２

---
+++ CampChallenge_java/011.メソッド/method03.java
@@ -0,0 +1,93 @@
+
+package method;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class method03 extends HttpServlet {
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet method3</title>");            
+            out.println("<body>");
+            
+            
+            out.print(abc(1,5,true,out));
+            
+            
+           //↑メソッドabcの呼び出し。
+
+            out.println("</body>");
+            out.println("</html>");
+            
+            
+            
+            
+        }
+        
+      
+  }
+      public double abc(double math,double math1,boolean math2,PrintWriter out){
+      
+      if(math2==true){
+           math= Math.pow(math * math1, 2);
+          return math;
+      }
+      else{
+          out.print(math * math1);
+      }
+      return math;
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
